cf6eaf3a1d83755afea207908b08d2c56c959625
MaxPlayFire <148088491+MaxPlayFire@users.noreply.github.com>
2025-10-11 13:17:32 +0300

    smert

--- notes/views.py
+++ notes/views.py
@@ -1,17 +1,21 @@
 from .models import Task
 from django.shortcuts import render, redirect
 from django.urls import path, reverse_lazy
-from django.views.generic import ListView, DetailView, CreateView, TaskForm
+from django.views.generic import ListView, DetailView, CreateView
+from .forms import TaskForm
+
 class TaskListView(ListView):
     model = Task
-    template_name = 'task_list.html'
+    template_name = 'notes/task_list.html'
     context_object_name = 'tasks'
+    
 class TaskDetailView(DetailView):
     model = Task
-    template_name = 'task_detail.html'
+    template_name = 'notes/task_detail.html'
     context_object_name = 'task'
+
 class TaskCreateView(CreateView):
     model = Task
     form_class = TaskForm
-    template_name = "task_form.html"
+    template_name = "notes/task_form.html"
     success_url = reverse_lazy("tasks:task_list")
